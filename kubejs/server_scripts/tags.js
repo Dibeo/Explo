6705d83d995500348d5f6d3bdaa2fed7b4999f8c
Dibeo <115413042+Dibeo@users.noreply.github.com>
2025-01-06 16:12:38 +0100

    craft, config and update

--- kubejs/server_scripts/tags.js
+++ kubejs/server_scripts/tags.js
@@ -2,4 +2,10 @@
  * @author dibeo
  *
  */
-ServerEvents.tags("item", (event) => {});
+ServerEvents.tags("item", (event) => {
+  const yelloriumItems = event.get("c:ingots/yellorium").getObjectIds();
+
+  yelloriumItems.forEach((item) => {
+    event.remove("c:ingots/uranium", item.toString());
+  });
+});
